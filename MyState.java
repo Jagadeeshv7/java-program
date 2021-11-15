60652f7edbc17c310ac56895a3107e0c1d2c424c
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2021-11-15 11:15:20 +0530

    Calling variable and method using object

---
+++ MyState.java
@@ -0,0 +1,17 @@
+public class MyState
+{       
+        public int a=3;
+        public String b="Java";
+	public static void main(String args[])
+	{
+                MyState TN=new MyState();
+		        TN.state(); 
+                System.out.println(TN.a);
+                System.out.println(TN.b);
+	}
+	public void state()
+	{
+		System.out.println("Tamil Nadu");
+	}
+}
+
