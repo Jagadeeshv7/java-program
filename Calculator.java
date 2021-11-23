a6c582648f463f996395f6d5183b688ac0bfc868
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2021-11-23 18:13:12 +0530

    shotcut for call a argumented method directly using object

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator
+{
+	public static void main(String args[])
+	{
+		Calculator calc=new Calculator();
+		System.out.println(new Calculator().add());
+	}
+	public int add()
+	{
+		return 5+10;
+	}
+}
