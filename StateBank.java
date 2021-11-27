ef638470f9a6021517184729e240e23b4e4ef16f
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2021-11-27 23:11:03 +0530

    Method definition

---
+++ StateBank.java
@@ -0,0 +1,16 @@
+public class StateBank
+{
+	int minBalance=100;
+	String custname;
+	public StateBank()
+	{
+	}
+	public void deposite()
+	{
+		System.out.println("Deposite");
+	}
+	public void withdraw()
+	{
+		System.out.println("Withdraw");
+	}
+}
