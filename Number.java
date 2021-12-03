cb259a0875c093266211033acb7430be5a9f2438
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2021-12-04 00:07:32 +0530

    While condition

---
+++ Number.java
@@ -0,0 +1,13 @@
+public class Number
+{
+	public static void main(String args[])
+	{
+		int i = 0;                                        
+                                                          
+		while (i < 5)                                                                                  
+		{                                                                     
+ 			 System.out.print(i);                                  
+ 			 i++;                                                    
+		}
+	}
+}
