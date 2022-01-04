000cd9f3e08785bcbf6614bf2f85dd35a5e9561f
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2022-01-04 22:40:24 +0530

    Second triangle with different row and colomn

---
+++ NumberPattern11.java
@@ -0,0 +1,15 @@
+public class NumberPattern11
+{
+	public static void main(String args[])
+	{
+		for (int i = 1; i <= 5; i++) {
+			for (int j = i + 1; j <= 5; j++) {
+				System.out.print(" " + " ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+	}
+}
