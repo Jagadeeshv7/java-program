f9e1d36213a24b2725901c09523713dea8e25ed3
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2022-01-08 23:26:29 +0530

    Third Triangle in row

---
+++ NumberPattern15.java
@@ -0,0 +1,15 @@
+public class NumberPattern15
+{
+	public static void main(String args[])
+	{
+		for (int i = 5; i >= 1; i--) {
+			for (int j = 5; j >= 1 + i; j--) {
+				System.out.print(" " + " ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print(k + " ");
+			}
+			System.out.println();
+		}
+	}
+}
