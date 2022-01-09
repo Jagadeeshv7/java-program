33e3aeaea8690186f8b48e948d4d46ff033e8066
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2022-01-09 23:44:00 +0530

    Reverse pyramid

---
+++ NumberPattern16.java
@@ -0,0 +1,18 @@
+public class NumberPattern16
+{
+	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j>1;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=i;k<=5;k++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
