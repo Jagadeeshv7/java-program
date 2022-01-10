4e22a22b03d2239262526d7bc3b23f24958714aa
Jagadeeshv7 <jagadeeshv3377@gmail.com>
2022-01-10 17:13:21 +0530

    pyramid

---
+++ NumberPattern17.java
@@ -0,0 +1,15 @@
+public class NumberPattern17
+{
+	public static void main(String args[])
+	{
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 5; j >= i; j--) {
+				System.out.print(" ");
+			}
+			for (int k = i; k >= 1; k--) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
